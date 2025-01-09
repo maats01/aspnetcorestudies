e899a8bdcf0f5f78f6561614498d12b28a794268
maats01 <matheusp.sampaio@gmail.com>
2025-01-09 16:09:23 -0300

    partial views exercises

---
+++ PartialViewsExample/PartialViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewsExample.Models;
+
+namespace PartialViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("programming-languages")]
+        public IActionResult ProgrammingLanguages()
+        {
+            ListModel listModel = new ListModel()
+            {
+                ListTitle = "Programming languages",
+                ListItems = new List<string>() { "Python", "C#", "Java", "C" }
+            };
+
+            return PartialView("_ListPartialView", listModel);
+        }
+    }
+}
