78c5ce426cb305cf950155bd655767370aefdc92
maats01 <matheusp.sampaio@gmail.com>
2025-02-05 16:17:29 -0300

    Environments and configuration exercises

---
+++ 13. Configuration/ConfigurationExample/ConfigurationExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ConfigurationExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly WeatherApiOptions _options;
+
+        public HomeController(IOptions<WeatherApiOptions> options)
+        {
+            _options = options.Value;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewBag.ClientId = _options.ClientId;
+            ViewBag.ClientSecret = _options.ClientSecret;
+
+            return View();
+        }
+    }
+}
---
+++ 13. Configuration/SocialMediaLinks/SocialMediaLinks/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace SocialMediaLinks.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SocialMediaLinksOptions _mediaOptions;
+
+        public HomeController(IOptions<SocialMediaLinksOptions> options)
+        {
+            _mediaOptions = options.Value;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(_mediaOptions);
+        }
+    }
+}
